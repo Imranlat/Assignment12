d4fb2e00b413e85785743a05e2ea7e8429330b0f
Imran latif <159417772+Imranlat@users.noreply.github.com>
2024-03-10 12:36:41 +0500

    Add files via upload

---
+++ q12.js
@@ -0,0 +1,6 @@
+"use strict";
+//12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let fNames = ["Ali", "Kamal", "Murad"];
+console.log(`Hi, ${fNames[0]}!`);
+console.log(`Hi, ${fNames[1]}!`);
+console.log(`Hi, ${fNames[2]}!`);
---
+++ q12.ts
@@ -0,0 +1,7 @@
+//12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let fNames: string[] = ["Ali", "Kamal", "Murad"];
+
+console.log(`Hi, ${fNames[0]}!`);
+console.log(`Hi, ${fNames[1]}!`);
+console.log(`Hi, ${fNames[2]}!`);
